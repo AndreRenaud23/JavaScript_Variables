9fc04dd18ec4c709a62bc74bd89168f53e326d82
AndreRenaud23 <andrer.mailbox@gmail.com>
2022-10-17 12:42:42 -0500

    Finished exercises

--- app.js
+++ app.js
@@ -5,6 +5,50 @@ console.log(
 
 // Exercise 1
 
+const firstName = "Andre";
+
+let lastName;
+
+var age;
+
 // Exercise 2
 
+// firstName = "Andre"; Cant re-declare a const
+
+lastName = "Renaud";
+
+age = 13;
+
 // Exercise 3
+
+let language = "JavaScript";
+
+let yearCreated = "1995";
+
+let isCaseSensitive = true;
+
+// Exercise 4
+
+let price = "19.99"
+
+let isOnSale = false;
+
+let salePercentage = 15
+
+let stock = 0;
+
+let inStock = false;
+
+let selectedSize = "M";
+
+// Exercise 5
+
+let title = "Name of the Wind";
+
+let author = "Patrick Rothfuss";
+
+let pageCount = 722;
+
+let bookmark = 456;
+
+let hasRead = true;
